972b557b150a7e6371fd6c3450e5e471bb978037
chaka <chakanov.111@mail.ru>
2023-02-23 14:52:58 +0300

    Add file

---
+++ Homework/13.8.py
@@ -0,0 +1,18 @@
+sum_tickets = int(input('Введите количество билетов: '))
+numbers_ages = []
+for i in range(0, sum_tickets):
+    input_value = int(input(f'Введите возраст посетителя №{i + 1}:\n'))
+    numbers_ages.append(input_value)
+    def prise(age):
+        if age < 18:
+            return 0
+        elif 18 <= age < 25:
+            return 990
+        else:
+            return 1390
+full_prise = sum(map(prise, numbers_ages))
+discount_prise = int(full_prise * 0.9)
+if sum_tickets > 3:
+    print('Сумма всех билетов со скидкой составляет: ', discount_prise, "руб.")
+else:
+    print('Стоимость всех билетов составляет: ', full_prise, "руб.")
--- Webinars/12_7_ТипыДанных.py
+++ Webinars/12_7_ТипыДанных.py
@@ -6,8 +6,8 @@
 # userName = 'Tom'
 # # snake case
 # user_name = 'Bill'
-
-
+#
+#
 # # DATE TYPES
 # # типизация: динамическая и статическая
 # # Java, C++ и тд.  string name = 'Ivan'
@@ -19,8 +19,8 @@
 # b = 5.3 + 10
 # print(a)
 # print(b)
-
-
+#
+#
 # # ДЕЛЕНИЕ
 # # / - обычное // - целочисленное % - остаток от деления
 # a = 5 / 2
@@ -29,13 +29,13 @@
 # print(a)
 # print(b)
 # print(с)
-
-
+#
+#
 # a = 10**3
 # print(a)
 # # возведение в степень
-
-
+#
+#
 # # ПРОВЕРКА на >/</==
 #
 # a = 8 > 3
@@ -44,8 +44,8 @@
 # print(b)
 # c = 8 < 3
 # print (c)
-
-
+#
+#
 # # ОКРУГЛЕНИЕ
 #
 # a  = round(10/3, 2)
@@ -56,28 +56,28 @@
 #
 # c = 10/3
 # print(c)
-
-
+#
+#
 # # ФУНКЦИЯ ПРЕОБРАЗОВАНИЯ - int() целочисленное преобразование
 # a = 10/3.0
 # b = int(a)
 # print(b)
-
-
+#
+#
 # # float() - вещественное преобразование(число с плавающей точкой)
 # # bin() - перевод из десятичного числа в двоичное
 #
 # print(bin(8))
-
-
+#
+#
 # import math
 # print(math.pow(2, 6))
 # # возведение в степень
 # print(math.sqrt(9))
 # # взять корень
 # print(math.pi)
-
-
+#
+#
 # # String
 # # '', "", ''' ''',
 # name = "Иван"
@@ -86,17 +86,17 @@
 # print(name[0])
 # print(name[2])
 # print(name[-1])
-
+#
 # name = "Иван"
 # fullname = "Иванович"
 # # срез строки [start:stop:step]
 # print (name[1:3])
 # print(name[1:6:2])
-
+#
 # concat
 # res = name + fullname + ' привет!'
 # print(res)
-
+#
 # # Methods string
 # string = 'Hello!'
 # res = string.lower()
@@ -105,80 +105,80 @@
 # res = string.upper()
 # print(res)
 # # делает все буквы заглавными
-
-
+#
+#
 # # делаем программу которая запрашивает данные на ввод имени и переформатирует текст в стандарт написания с загл буквы
 # name = input('Введите Ваше имя ')
 # name_start = name[0].upper()
 # name_end = name[1:].lower()
 # res = name_start + name_end
 # print(res)
-
-
+#
+#
 # # for..in
 #
 # name = 'Ivan'
 # for i in name:
 #     print(i)
 # # сделали перебор всех символов с новой строчки
-
+#
 # exm = 'Привет мир'
 # count = 0 # сделали счетчик
 # for i in exm:
 #     if i == 'и':
 #         count = count + 1 # счетчик добавляет +1 каждый раз когда находит символ "и"
 # print(count, 'буквы "и" данной строке')
-
-
+#
+#
 # # проверка на вхождения
 # # in и not in
 # a = 'hello'
 # b = 'hello world'
 # print(a in b)
-
-
+#
+#
 # # len() показывает сумму символов в строке
 # string = 'hello'
 # print(len(string))
-
+#
 # # split() разбивает строки по разделителю
 # # join() собирает строку из списка
-
+#
 # #\n переход на следующую строчку
 # print('hello\nworld')
-
-
+#
+#
 # # format
 # #{} можно задать порядок по ключевым словам
 #
 # string = "{a},{b},{c}".format(a='Петя', c='Ваня',b='Катя')
 # print(string)
-
-
+#
+#
 # # F - СТРОКИ
-
+#
 # # простой синтаксис
 # name = 'Eric'
 # age = 24
 #
 # print(f'Hello, {name}. You are {age}.')
-
-
+#
+#
 # print(f"{2*37}")
-
-
+#
+#
 # # List - [], list()
 # numbers = [1,2,3,4, 'hello', True]
 # numbers_1 = list()
 # print(numbers_1)
 # print(numbers)
-
-
+#
+#
 # people = ['Ivan', 'Tom', 'Kate']
 # for person in people:
 #     print(person)
-
-
+#
+#
 # #Methods list
 #
 # #append(item) добавляет элемент в список
@@ -189,42 +189,42 @@
 # #remove(item) удаляет элемент(только первый попавшийся
 # print(people.remove('Bill'))
 # print(people)
-
+#
 # # in
 # people = ['Ivan', 'Tom', 'Kate']
 # if 'Tom' in people:
 #     people.remove('Tom')
 # print(people)
-
-
+#
+#
 # #count()
 # people = ['Ivan', 'Tom', 'Kate','Tom']
 # people_count = people.count('Tom')
 # print(people_count)
-
-
+#
+#
 # a = [4,3,2,1]
 # print(sorted(a)) #сортирует по порядку
-
+#
 # people = [
 #     ['Ivan', 29],
 #     ['Kate', 31],
 #     ['Bob',45]
 #
 # print(people[0][1]) #выводим второй элемент из первого списка
-
-
+#
+#
 # Tuple () похож на список но отличается тем что он является неизменяемым
-
+#
 # # СЛОВАРИ {keys:values}
 # users = {1:'Tom',2:'Bob',3:'Kate'}
 # users[1]='Tom2'
 # print(users)
-
+#
 # users = {1:'Tom',2:'Bob',3:'Kate'}
 # users_1 = users.get(3)
 # print(users_1) #вытаскиваем значение через обращние по ключу
-
+#
 # users = {1:'Tom',2:'Bob',3:'Kate'}
 # # items()
 # for key, value in users.items():
@@ -234,7 +234,7 @@
 #
 # users = {'Tom','Bob','Kate'}
 # print(users) # каждый раз выводит в разной последовательности, все элементы уникальны
-
+#
 # # union() - объединение
 # users_1 = {'Tom','Bob','Kate'}
 # users_2 = {'Sam','Kirill','Alex'}
