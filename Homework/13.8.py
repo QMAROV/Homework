1d28b912363750a7258d5ae13f22306f8b02efef
chaka <chakanov.111@mail.ru>
2023-02-24 14:12:19 +0300

    Add file

--- Homework/13.8.py
+++ Homework/13.8.py
@@ -14,10 +14,6 @@ numbers_ages = []
 for i in range(0, sum_tickets):
     input_value = int(input(f'Введите возраст посетителя №{i + 1}:\n'))
     numbers_ages.append(input_value)
-if sum_tickets == 0:
-    print('Количество посетителей должно быть минимум 1 человек')
-
-
     def prise(age):
         if age < 18:
             return 0
@@ -27,6 +23,7 @@ if sum_tickets == 0:
             return 1390
 full_prise = sum(map(prise, numbers_ages))
 discount_prise = int(full_prise * 0.9)
+
 if sum_tickets > 3:
     print('Сумма всех билетов со скидкой составляет: ', discount_prise, "руб.")
 else:
